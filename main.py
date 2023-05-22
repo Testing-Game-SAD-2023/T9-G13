76da139ad8f42f48df488f94381d6868f316a6e1
annaamodio <anna.amodio@studenti.unina.it>
2023-05-22 10:50:36 +0200

    modifica script

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import random
 coverage = 0
 
 
-file_path = sys.argv
-input_classname = "Calcolatrice"
-junit_path = "./src/test/java/" + input_classname
-
+# IMPOSTARE CORRETTAMENTE I PATH
+input_classname = sys.argv
+#junit_path = "./src/test/java/" + input_classname
+project_dir = "./"
+main_dir = project_dir + "src/main/java"
+test_dir = project_dir + "src/test/java"
 shared_dir = "../shared_dir"
 class_path_base = shared_dir + "/class_to_test"
-end_path_base = shared_dir + "/generated_tests"
+end_path_base = shared_dir + "/"
 
 def randoop(input_classname, time_limit, nomeregr, nomeerr, seed, junit_path):
     os.system("mvn compile")
@@ -22,24 +24,32 @@ def randoop(input_classname, time_limit, nomeregr, nomeerr, seed, junit_path):
 
 
 def runtest(input_classname, junit_path):
+   
     i = 0
     test_for_section = 0
     dir_num = 0
     coverage = 0
     old_coverage = 0
-    I_MAX = 50
+    max_test_for_session = 5
+    I_MAX = 50  # valutare il valore di questo parametro
     DELTA = 0.05
-    MAX_COVERAGE = 0.90
-    while coverage < MAX_COVERAGE and i != I_MAX:
+    time = 5
+    # MAX_COVERAGE = 0.90
+    while i < I_MAX:
         if test_for_section == 0:
+            # queste operazioni devono essere svolte nella cartella src/test/java/
             os.system("mkdir " + "session_" + dir_num)
             os.system("cd ")
+            dir_num = dir_num + 1
 
         nomeerr = "Error" + str(i)
         nomeregr = "Regression" + str(i)
 
         randoop(input_classname, 5, nomeregr, nomeerr, i * random.randint(1, 100), junit_path)
-
+        time = increment_time(time)
+        
+        test_for_section = (test_for_section + 1) % max_test_for_session
+           
         csvf = pd.read_csv("./target/my-reports/jacoco.csv")
         inst_missed = csvf["INSTRUCTION_MISSED"]
         inst_covered = csvf["INSTRUCTION_COVERED"]
@@ -55,14 +65,26 @@ def runtest(input_classname, junit_path):
 
 
 def init_test(input_classname):
-    dir_name = str(input_classname).split('.')[0]
+    os.system("mvn clean")
+    class_name = str(input_classname).split('.')[0]
+    dir_name = class_name.lower + "_randoop"
     os.system("cd " + end_path_base)
     os.system("mkdir " + dir_name)
-    os.system("cp " + class_path_base + "/" + input_classname + " ./target/classes/")
-    
-    #os.system("cd " + end_path_base + dir_name)
+    os.system("cd " + project_dir)
+    os.system("cp " + class_path_base + "/" + input_classname + " "+main_dir)
 
 
-def start_test(input_classname):
+def start_test(input_classnamey):
     init_test(input_classname)
     runtest(input_classname, junit_path)
+
+
+def increment_time(time):
+    # parametri da definire
+    TIME_MAX = 100
+    TIME_INCR = 5
+    if(time < TIME_MAX):
+        time+=TIME_INCR
+    else:
+        time+=1
+    return time
