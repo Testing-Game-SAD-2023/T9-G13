7b104b68890e0e85c5adeea82755f7b78640de41
Salvatore Bramante <salvatore.bramante@yahoo.it>
2023-06-13 11:09:43 +0200

    clean up

--- T9_interface/src/main/java/admin/Observer.java
+++ T9_interface/src/main/java/admin/Observer.java
@@ -13,23 +13,16 @@ import java.io.IOException;
 public class Observer implements IObserver {
     public void notifyCompleted (int numberSession){
         System.out.println("[OBSERVER]: "+ numberSession +" test sessions generated");
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("/home/tesi-step/Desktop/git/T9-G13/output.txt"));
-            writer.write("number of sessions: "+numberSession);
-            writer.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static void main(String[] argv){
         IRandoopConnector instance = RandoopConnector.getInstance();
         Observer ob = new Observer();
         try {
-            //instance.generateRandoopTest("Calcolatrice", 7,ob) ;
-            //System.out.println("Richiesta per Calcolatrice inoltrata");
-            instance.generateRandoopTest("VCardBean",2, ob) ;
-            System.out.println("Richiesta per VCardBean inoltrata");
+            instance.generateRandoopTest("Calcolatrice", 7,ob) ;
+            System.out.println("Richiesta per Calcolatrice inoltrata");
+            //instance.generateRandoopTest("VCardBean",2, ob) ;
+            //System.out.println("Richiesta per VCardBean inoltrata");
         }catch (RandoopException e){
             e.printStackTrace();
         }
