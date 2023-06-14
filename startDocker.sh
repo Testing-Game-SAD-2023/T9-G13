bc05340186d74d98ebda6677e9c45035ef772837
Salvatore Bramante <salvatore.bramante@yahoo.it>
2023-06-14 19:38:59 +0200

    fix

--- startDocker.sh
+++ startDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo chmod 666 /var/run/docker.sock &&
 docker build -t randoop-container . &&
-docker run -i -t -v $1/:/root/repo --dns 8.8.8.8 --name T9 randoop-container
+docker run -d -v $1/:/root/repo --dns 8.8.8.8 --name T9 randoop-container
 #docker pause T9
