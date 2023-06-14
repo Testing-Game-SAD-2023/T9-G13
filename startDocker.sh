bd3a0f708525e63af10f5ebdc4dee7bbbf0f6723
stepg23 <stefanog2301@gmail.com>
2023-06-14 21:04:31 +0200

    script per windows

--- startDocker.sh
+++ startDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo chmod 666 /var/run/docker.sock &&
 docker build -t randoop-container . &&
-docker run -d -v $1/:/root/repo --dns 8.8.8.8 --name T9 randoop-container
+docker run -t -i -v $1/:/root/repo --dns 8.8.8.8 --name T9 randoop-container
 #docker pause T9
