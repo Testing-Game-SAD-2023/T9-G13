787759d9ab977ec152451d10696185cd587fe516
Salvatore Bramante <salvatore.bramante@yahoo.it>
2023-06-16 12:09:23 +0200

    test all

--- esempio/src/main/java/org/example/Main.java
+++ esempio/src/main/java/org/example/Main.java
@@ -4,19 +4,31 @@ import exceptions.RandoopException;
 import interfaces.IObserver;
 import interfaces.IRandoopConnector;
 import randoop.RandoopConnector;
-
+import java.io.File;
 public class Main implements IObserver {
 
     public static void main(String[] args) {
         Main main = new Main();
-        IRandoopConnector randoopConnector = RandoopConnector.getInstance();
-        try {
-            randoopConnector.generateRandoopTest("Calcolatrice", 2, main);
-        }catch (RandoopException e){
-            e.printStackTrace();
-            System.out.println("ERROR CODE: "+e.getERROR_TYPE());
-        }
 
+        String path = System.getProperty("user.home") + args[0];
+
+        RandoopConnector conn = RandoopConnector.getInstance();
+        conn.setRepositoryDir(path);
+
+        IRandoopConnector randoopConnector = RandoopConnector.getInstance();
+
+        File currentDir = new File(path);
+
+        File[] folders = currentDir.listFiles(File::isDirectory);
+
+        for (File folder : folders) {
+            try {
+                randoopConnector.generateRandoopTest(folder.getName(), 3, main);
+            }catch (RandoopException e){
+                e.printStackTrace();
+                System.out.println("ERROR CODE: "+e.getERROR_TYPE());
+            }
+        }
     }
 
     @Override
